fb844084d1bc9a0e621337db5c62dde954d9f223
lucasamsilva <lucasandreim280998@gmail.com>
2021-04-07 13:00:34 -0300

    Frequencia dados

---
+++ PreProcessamento/Histograma.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+input_file = '../Dataset/horse-colic-clean.data'  # Importação dos Dados
+df = pd.read_csv(input_file, usecols=['Temperatura Retal', 'Pulso'])
+temperaturas = df['Temperatura Retal'].tolist()
+pulso = df['Pulso'].tolist()
+
+plt.title('Temperatura cavalos com cólicas')
+plt.xlabel('Temperatura')
+plt.ylabel('Quantidade')
+plt.hist(temperaturas, 15, rwidth=0.9, edgecolor='black')
+
+plt.show()
+
+plt.title('Pulso cavalos com cólicas')
+plt.xlabel('Pulso')
+plt.ylabel('Quantidade')
+plt.hist(pulso, 15, rwidth=0.9, edgecolor='black')
+
+plt.show()
